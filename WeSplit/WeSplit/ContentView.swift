34c9af3b1d37fb13b39db72a3a7f25fcf9764a8c
Arjav Lad <arjav.webexcellis@gmail.com>
2020-05-06 15:43:55 +0530

    Add a header to the third section "Amount per person"

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,31 +9,51 @@
 import SwiftUI
 
 struct ContentView: View {
-	@State private var tapCount = 0
-	@State private var storedText: String = ""
+	@State private var checkAmount = ""
+	@State private var numberOfPeople = 2
+	@State private var tipPercentage = 2
+
+	let tipPercentages = [10, 15, 20, 25, 0]
+	var orderAmount: Double { Double(checkAmount) ?? 0 }
+	var tipSelection: Double { Double(tipPercentages[tipPercentage]) }
+	var tipValue: Double { orderAmount / 100 * tipSelection }
+
+	var totalPerPerson: Double {
+		let peopleCount = Double(numberOfPeople + 2)
+		let amountPerPerson = grandTotal / peopleCount
+		return amountPerPerson
+	}
+
+	var grandTotal: Double { orderAmount + tipValue }
+
 	var body: some View {
 		NavigationView {
 			Form {
-				Group {
-					Button("Tap Count: \(tapCount)") {
-						self.tapCount += 1
-					}
-					Button("Clear Taps") {
-						self.tapCount = 0
+				Section {
+					TextField("Enter Amount", text: $checkAmount)
+						.keyboardType(.decimalPad)
+					Picker("Number of people", selection: $numberOfPeople) {
+						ForEach(2 ..< 100) {
+							Text("\($0) people")
+						}
 					}
 				}
-				Group {
-					Text("Hello")
-					Text("Hello")
-					Text("Hello")
-					Text("Hello")
-					Text("Hello")
-					Text("Hello")
-					Text(storedText)
-					TextField("Enter Text Here", text: $storedText).keyboardType(.numberPad)
+				Section(header: Text("How much tip do you want to leave?")) {
+					Picker("Tip percentage", selection: $tipPercentage) {
+						ForEach(0 ..< tipPercentages.count) {
+							Text("\(self.tipPercentages[$0])%")
+						}
+					}.pickerStyle(SegmentedPickerStyle())
 				}
-			}
-			.navigationBarTitle("List", displayMode: .inline)
+				Section(header: Text("Amount per person")) {
+					Text("$ \(totalPerPerson, specifier: "%.2f")")
+				}
+
+				Section(header: Text("Total amount for check")) {
+					Text("$ \(grandTotal, specifier: "%.2f")")
+				}
+
+			}.navigationBarTitle("WeSplit")
 		}
 	}
 }
